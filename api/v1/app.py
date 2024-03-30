64ba96313ba61cbafd006f3e4390f7c5722d457d
ASLawan <nawalnitsua@gmail.com>
2024-03-31 00:47:01 +0100

    Fix pycodestyle errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,7 +24,6 @@ def teardown_storage(exception):
 if __name__ == "__main__":
     HBNB_API_HOST = os.getenv('HBNB_API_HOST')
     HBNB_API_PORT = os.getenv('HBNB_API_PORT')
-    
     host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
     port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
