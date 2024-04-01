6a0f66b31dad68cccd45fa58f00809215518771a
ASLawan <nawalnitsua@gmail.com>
2024-04-01 18:43:17 +0100

    Return number of instances of each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,13 +4,14 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns status code in JSON"""
     return jsonify(status='OK')
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """returns number of instances of each object class"""
     classes = ["Amenity", "City", "Place", "Review", "State", "User"]
