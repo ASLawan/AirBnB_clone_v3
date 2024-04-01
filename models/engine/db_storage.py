e714184c0d02f05681e12f57f281881410c9dc06
ASLawan <nawalnitsua@gmail.com>
2024-04-01 20:10:42 +0100

    Update sb storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,9 +77,10 @@ class DBStorage:
 
     def get(self, cls, id):
         """returns object based on class and id or None if not found"""
-        if cls and cls in classes and id:
-            obj = self.session.query(classes['cls']).filter_by(id=id).first()
-            return obj
+        objs = self.__session.query(classes[cls])
+        for obj in objs:
+            if obj.id == id:
+                return obj
         return None
 
     def count(self, cls=None):
@@ -87,6 +88,6 @@ class DBStorage:
         if none is given"""
         num_objs = 0
         for clas in classes:
-            if cls is None or cls in classes or cls is clas:
+            if cls is None or cls in classes[clas] or cls is clas:
                 num_objs += len(self.__session.query(classes[clas]).all())
         return num_objs
