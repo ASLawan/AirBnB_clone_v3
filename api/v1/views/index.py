e179016b9e0a827af3731b8f084a2773f13089d9
ASLawan <nawalnitsua@gmail.com>
2024-04-01 17:57:44 +0100

    Modify index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,22 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """returns status code in JSON"""
     return jsonify(status='OK')
+
+
+@app_views.route('/status', methods=['GET'])
+def get_stats():
+    """returns number of instances of each object class"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    stats = {}
+
+    for cls in classes:
+        cls_count = storage.count(cls)
+        stats[cls.lower()] = cls_count
+    return jsonify(stats)
+
